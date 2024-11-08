29b33b524de5facb485e3512fe861d149213e712
phunguyen <phunt0391@gmail.com>
2024-11-08 11:13:22 +0700

    update note.md

--- src/test/java/theInternet/HoverTest.java
+++ src/test/java/theInternet/HoverTest.java
@@ -8,20 +8,27 @@ import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.*;
 
 public class  HoverTest {
+    WebDriver driver;
 
-    @Test
-    void ableToHoverImage1() {
-        WebDriver driver = new ChromeDriver();
+    @BeforeMethod
+    void setUp(){
+        driver = new ChromeDriver();
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments(("--headless=new"));
 
         driver.get("https://the-internet.herokuapp.com/hovers");
+    }
 
+    @Test
+    void ableToHoverImage1() {
         Actions actions = new Actions(driver);
 
         actions
                 .moveToElement(driver.findElements(By.cssSelector("#content .figure"))
                         .get(0)).perform();
+    }
+    @AfterMethod
+    void tearDown(){
         driver.quit();
     }
 }
