650b50db31027ba19b0d06cf97300e3d1d51e1d2
phunguyen <phunt0391@gmail.com>
2024-11-08 11:22:53 +0700

    update HoverTest.java

--- src/test/java/theInternet/HoverTest.java
+++ src/test/java/theInternet/HoverTest.java
@@ -7,11 +7,11 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.*;
 
-public class  HoverTest {
+public class HoverTest {
     WebDriver driver;
 
     @BeforeMethod
-    void setUp(){
+    void setUp() {
         driver = new ChromeDriver();
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments(("--headless=new"));
@@ -22,13 +22,13 @@ public class  HoverTest {
     @Test
     void ableToHoverImage1() {
         Actions actions = new Actions(driver);
-
         actions
                 .moveToElement(driver.findElements(By.cssSelector("#content .figure"))
                         .get(0)).perform();
     }
+
     @AfterMethod
-    void tearDown(){
+    void tearDown() {
         driver.quit();
     }
 
