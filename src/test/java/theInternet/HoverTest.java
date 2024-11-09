78e1c22fe05204fb89c63ba3f3a47a551981fb64
phunguyen <phunt0391@gmail.com>
2024-11-09 10:33:47 +0700

    add new code in HoverTest.java

--- src/test/java/theInternet/HoverTest.java
+++ src/test/java/theInternet/HoverTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.*;
 
 public class HoverTest {
@@ -16,13 +17,49 @@ public class HoverTest {
         chromeOptions.addArguments("--headless=new");
 
         driver = new ChromeDriver(chromeOptions);
+        driver.get("https://the-internet.herokuapp.com/hovers");
     }
 
     @Test
     void ableToHoverImage1() {
-        driver.get("https://the-internet.herokuapp.com/hovers");
+
         Actions actions = new Actions(driver);
-        actions.moveToElement(driver.findElements(By.cssSelector("#content .figure")).get(0)).perform();
+
+        actions
+                .moveToElement(driver.findElements(By.cssSelector("#content .figure"))
+                        .get(0))
+                .perform();
+    }
+    @Test
+    void ableToHoverImage2(){
+
+        Actions actions = new Actions(driver);
+
+        actions
+                .moveToElement(
+                        driver
+                                .findElements(By.cssSelector("#content .figure"))
+                                .get(1))
+                .perform();
+
+        String imageName = driver.findElements(By.cssSelector(".figcaption h5")).get(1).getText();
+        Assert.assertEquals(imageName,"name: user2");
+    }
+
+    @Test
+    void ableToHoverImage3(){
+
+        Actions actions = new Actions(driver);
+
+        actions
+                .moveToElement(
+                        driver
+                                .findElements(By.cssSelector("#content .figure"))
+                                .get(2))
+                .perform();
+
+        String imageName = driver.findElements(By.cssSelector(".figcaption h5")).get(2).getText();
+        Assert.assertEquals(imageName,"name: user3");
     }
 
     @AfterMethod
