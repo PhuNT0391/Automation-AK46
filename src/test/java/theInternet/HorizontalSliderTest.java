ac94759f446e5d2184a1ce56c6b73e6f33983473
phunguyen <phunt0391@gmail.com>
2024-11-12 10:38:48 +0700

    modified DragDropTest, HorizontalSliderTest and webtable-testplan

---
+++ src/test/java/theInternet/HorizontalSliderTest.java
@@ -0,0 +1,21 @@
+package theInternet;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class HorizontalSliderTest {
+    @Test
+    void ableToHorizontalSlidePointer() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/horizontal_slider");
+        Actions actions = new Actions(driver);
+        WebElement pointer = driver.findElement(By.cssSelector());
+
+    }
+
+}
+
