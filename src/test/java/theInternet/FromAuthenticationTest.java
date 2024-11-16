0ecaead60c0fd44950861304ca62ce4f8ecad6db
phunguyen <phunt0391@gmail.com>
2024-11-16 13:34:19 +0700

    update new code for FromAuthenticationTest.java

--- src/test/java/theInternet/FromAuthenticationTest.java
+++ src/test/java/theInternet/FromAuthenticationTest.java
@@ -29,38 +29,36 @@ public class FromAuthenticationTest {
     void tearDown() {
         Browser.quit();
     }
-
-    @Test
-    void shouldBeFailedWithInvalidUsername() {
-        driver.get("https://the-internet.herokuapp.com/login");
-
-        driver.findElement(By.id("username")).sendKeys("tomsmith123");
-        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
-        driver.findElement(By.cssSelector("button[type=submit]")).click();
-
-        Assert.assertTrue(driver.findElement(By.className("error")).getText().contains("Your username is invalid!"));
-    }
-
-    @Test
-    void shouldBeFailedWithInvalidPassword() {
-        driver.get("https://the-internet.herokuapp.com/login");
-
-        driver.findElement(By.id("username")).sendKeys("tomsmith");
-        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!123");
-        driver.findElement(By.cssSelector("button[type=submit]")).click();
-
-        Assert.assertTrue(driver.findElement(By.className("error")).getText().contains("Your password is invalid!"));
-    }
-
-    @Test
-    void backLoginPage() {
-        driver.get("https://the-internet.herokuapp.com/login");
-
-        driver.findElement(By.id("username")).sendKeys("tomsmith");
-        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
-        driver.findElement(By.cssSelector("button[type=submit]")).click();
-
-        driver.findElement(By.cssSelector("a[class='button secondary radius']")).click();
-        Assert.assertTrue(driver.findElement(By.className("success")).getText().contains("You logged out of the secure area!"));
-    }
+//
+//    @Test
+//    void shouldBeFailedWithInvalidUsername() {
+//        FormAuthenticationPage formAuthenticationPage = new FormAuthenticationPage();
+//        formAuthenticationPage.open();
+//        formAuthenticationPage.login("tomsmith123", "SuperSecretPassword!");
+//
+//        Assert.assertTrue(driver.findElement(By.className("error")).getText().contains("Your username is invalid!"));
+//    }
+//
+//    @Test
+//    void shouldBeFailedWithInvalidPassword() {
+//        driver.get("https://the-internet.herokuapp.com/login");
+//
+//        driver.findElement(By.id("username")).sendKeys("tomsmith");
+//        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!123");
+//        driver.findElement(By.cssSelector("button[type=submit]")).click();
+//
+//        Assert.assertTrue(driver.findElement(By.className("error")).getText().contains("Your password is invalid!"));
+//    }
+//
+//    @Test
+//    void backLoginPage() {
+//        driver.get("https://the-internet.herokuapp.com/login");
+//
+//        driver.findElement(By.id("username")).sendKeys("tomsmith");
+//        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+//        driver.findElement(By.cssSelector("button[type=submit]")).click();
+//
+//        driver.findElement(By.cssSelector("a[class='button secondary radius']")).click();
+//        Assert.assertTrue(driver.findElement(By.className("success")).getText().contains("You logged out of the secure area!"));
+//    }
 }
